704e5f98aea7c4c8fa0727820f216cf24e574cd2
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-03-31 14:27:45 -0300

    Adiciona outline de EntityCard

---
+++ src/components/EntityCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import EditButton from 'src/components/EditButton'
+
+const EntityCard : React.FC = () => {
+
+    return (
+        <div>
+            <EditButton/>
+        </div>
+    )
+
+}
+
+export default EntityCard
