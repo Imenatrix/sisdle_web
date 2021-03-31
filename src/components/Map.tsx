052a6bdb91d6c596e24e404349da293835a04f4c
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-03-31 14:12:18 -0300

    Adiciona outline de Map

---
+++ src/components/Map.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Map : React.FC = () => {
+
+    return (
+        <div>
+            // aqui vai o mapa (leaflet, Mapbox GL JS, etc)
+        </div>
+    )
+
+}
+
+export default Map 
