c6192eb66fa98e47efda0870eadfb09a5ce78a44
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-03-31 14:56:58 -0300

    Adiciona outline de Tabs

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Tabs : React.FC = () => {
+
+    return (
+        <nav>
+            {
+                // lista as entidades para busca em SearchCard
+            }
+        </nav>
+    )
+
+}
+
+export default Tabs
