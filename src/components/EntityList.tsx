d06811333ac662dc3de576d60c446266caaeaf11
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-03-31 15:02:42 -0300

    Adiciona outline de EntityList

---
+++ src/components/EntityList.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const EntityList : React.FC = () => {
+
+	return (
+		<ul>
+			{
+				// lista dos resultados de pesquisa de SearchCard
+			}
+		</ul>
+	)
+}
+
+export default EntityList
