e6fb5e6422402880ba3f88162cab22a5de986ead
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-03-31 18:53:50 -0300

    Define estilo de App

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import Map from 'src/components/Map'
 import SearchCard from 'src/components/SearchCard'
 import EntityCard from 'src/components/EntityCard'
+import { createUseStyles } from 'react-jss'
 
 const App : React.FC = () => {
 
+	const styles = useStyles()
+
 	return (
-		<div>
+		<div className={styles.container}>
 			<Map/>
 			<SearchCard/>
 			<EntityCard/>
@@ -16,3 +19,14 @@ const App : React.FC = () => {
 }
 
 export default App
+
+const useStyles = createUseStyles({
+	'@global body' : {
+		margin : 0
+	},
+	'container' : {
+		'display' : 'flex',
+		'width' : '100vw',
+		'height' : '100vh'
+	}
+})
