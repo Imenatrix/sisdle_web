3260e929b0c3e7b9a848817349ac3e3c45c90741
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-03-31 14:35:45 -0300

    Adiciona outline de EditButton

---
+++ src/components/EditButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const EditButton : React.FC = () => {
+
+    // deve mudar entre visualização e formulario de edição
+    // em EntityCard
+    return (
+        <button>
+            // botão azul com icone de lapis para edição
+        </button>
+    )
+
+}
+
+export default EditButton
