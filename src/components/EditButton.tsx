7534dea91482f00e849a7221a919d46bdbff796d
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-04-01 17:34:58 -0300

    Define estilos de EditButton

--- src/components/EditButton.tsx
+++ src/components/EditButton.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { createUseStyles } from 'react-jss'
 
 const EditButton : React.FC = () => {
 
+	const styles = useStyles()
 	// deve mudar entre visualização e formulario de edição
 	// em EntityCard
 	return (
-		<button>
+		<button className={styles.btn}>
 			{
 				// botão azul com icone de lapis para edição
 			}
@@ -15,3 +17,20 @@ const EditButton : React.FC = () => {
 }
 
 export default EditButton
+
+const useStyles = createUseStyles({
+	btn : {
+		backgroundColor : '#2196F3',
+		border : 'none',
+		width : '3em',
+		height : '3em',
+		padding : 0,
+		borderRadius : '1.5em',
+		'&:focus' : {
+			outline : 'none'
+		},
+		'&:active' : {
+			opacity : 0.9,
+		}
+	}
+})
