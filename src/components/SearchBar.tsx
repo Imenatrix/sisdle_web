74a128799ada39bc9d1d452f58936daf0b069fac
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-04-01 15:47:51 -0300

    Define estilos para SearchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,48 @@
 import React from 'react'
+import { createUseStyles } from 'react-jss'
 
 const SearchBar : React.FC = () => {
 
+	const styles = useStyles()
+
 	return (
-		<div>
-			<input type="text"/>
-			<button/>
+		<div className={styles.container}>
+			<input type="text" className={styles.txtSearch}/>
+			<button className={styles.btnSearch}/>
 		</div>
 	)
 }
 
 export default SearchBar
+
+const useStyles = createUseStyles({
+	container : {
+		display : 'flex'
+	},
+	txtSearch : {
+		flex : 1,
+		border : 'none',
+		borderTopLeftRadius : '100vmax',
+		borderBottomLeftRadius : '100vmax',
+		paddingLeft : '0.5em',
+		'&:focus' : {
+			outline : 'none',
+			border : ['solid', 'lightgray', 2]
+		}
+	},
+	btnSearch : {
+		height : '1.7em',
+		border : 'none',
+		aspectRatio : 1,
+		padding : 0,
+		backgroundColor : '#2196F3',
+		borderTopRightRadius : '100vmax',
+		borderBottomRightRadius : '100vmax',
+		'&:active' : {
+			opacity : 0.9
+		},
+		'&:focus' : {
+			outline : 'none'
+		}
+	}
+})
