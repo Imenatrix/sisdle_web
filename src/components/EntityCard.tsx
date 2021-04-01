473805d2ad7e6ee52fdee2be124acafa7cac600d
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-04-01 16:55:58 -0300

    Define estilos de EntityCard

--- src/components/EntityCard.tsx
+++ src/components/EntityCard.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import EditButton from 'src/components/EditButton'
+import { createUseStyles } from 'react-jss'
 
 const EntityCard : React.FC = () => {
 
+	const styles = useStyles()
+
 	return (
-		<div>
+		<div className={styles.container}>
 			<EditButton/>
 		</div>
 	)
@@ -12,3 +15,13 @@ const EntityCard : React.FC = () => {
 }
 
 export default EntityCard
+
+const useStyles = createUseStyles({
+	container : {
+		backgroundColor : 'green',
+		flex : 0.333,
+		margin : ['1em', 0],
+		borderTopRightRadius : '0.85em',
+		borderBottomRightRadius : '0.85em'
+	}
+})
