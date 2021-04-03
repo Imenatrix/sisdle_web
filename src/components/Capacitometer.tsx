83cc276457495ec265946000f2eb8d47351fb92a
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-04-03 17:03:19 -0300

    Define estilos de Capacitometer

--- src/components/Capacitometer.tsx
+++ src/components/Capacitometer.tsx
@@ -1,14 +1,35 @@
 import React from 'react'
+import { createUseStyles, Styles } from 'react-jss'
 
 const Capacitometer : React.FC = () => {
 
+	const styles = useStyles({capacity : 75})
+	
 	return (
-		<div>
-			<div/>
-			<div/>
+		<div className={styles.container}>
+			<div className={styles.empty}/>
+			<div className={styles.full}/>
 		</div>
 	)
 
 }
 
 export default Capacitometer
+
+// TODO: generalizar sem precisar por o nome das classes
+const useStyles = createUseStyles<'container' | 'empty' | 'full', {capacity : number}>({
+	container : {
+		display : 'flex',
+		flexDirection : 'column',
+		flex : 0.1,
+		boxShadow: [0, 0, 5, 'rgba(0, 0, 0, 0.3)'],
+	},
+	empty : {
+		flex : props => 100 - props.capacity,
+		backgroundColor : 'white'
+	},
+	full : {
+		flex : props => props.capacity,
+		backgroundColor : 'green'
+	}
+})
