b885a16d1ec9d59b3e8e972c9495d90700a29853
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-04-03 17:03:38 -0300

    Define estilos de LixeiraPod

--- src/components/LixeiraPod.tsx
+++ src/components/LixeiraPod.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Capacitometer from 'src/components/Capacitometer'
+import { createUseStyles } from 'react-jss'
 
 const LixeiraPod : React.FC = () => {
 
+	const styles = useStyles()
+
 	return (
-		<div>
+		<div className={styles.container}>
 			<Capacitometer/>
-			<div>
+			<div className={styles.txtContainer}>
 				<div>Lorem, ipsum.</div>
 				<div>Lorem ipsum dolor sit.</div>
 			</div>
@@ -16,3 +19,17 @@ const LixeiraPod : React.FC = () => {
 }
 
 export default LixeiraPod
+
+const useStyles = createUseStyles({
+	container : {
+		overflow : 'hidden',
+		display : 'flex',
+		borderRadius : '0.85em',
+		boxShadow: [0, 0, 10, 'rgba(0, 0, 0, 0.3)'],
+		margin : ['1em', 0]
+	},
+	txtContainer : {
+		flex : 1,
+		padding : '0.5em'
+	}
+})
