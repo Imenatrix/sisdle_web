0b01f943acc98962336f3b47bf0d08d323e553c4
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-04-03 13:47:15 -0300

    Cria outline de LixeiraPod

---
+++ src/components/LixeiraPod.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Capacitometer from 'src/components/Capacitometer'
+
+const LixeiraPod : React.FC = () => {
+
+	return (
+		<div>
+			<Capacitometer/>
+			<div>
+				<div>Lorem, ipsum.</div>
+				<div>Lorem ipsum dolor sit.</div>
+			</div>
+		</div>
+	)
+
+}
+
+export default LixeiraPod
