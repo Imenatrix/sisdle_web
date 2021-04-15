80fb75a6203c1dfcffda0af94d8796901b508eb6
BuildTools <unconfigured@null.spigotmc.org>
2021-04-15 18:28:30 -0300

    Adiciona servidor customizado
    Co-authored-by: Ruan Gomes <ruangomesdorosario@gmail.com>

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const next = require('next')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const nextapp = next({ dev })
+const handle = nextapp.getRequestHandler()
+
+nextapp.prepare().then(() => {
+    const app = express()
+
+    require('src/api/controllers/authController')(app);
+
+    app.listen(port, (err) => {
+        if (err) throw err
+        console.log(`> Servidor iniciado em: http://localhost:${port}`)
+    })
+})
