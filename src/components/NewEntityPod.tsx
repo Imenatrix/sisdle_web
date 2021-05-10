766561b03d509f3c02046defb1fee73d4d287672
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-05-10 17:19:31 -0300

    Adiciona NewEntityPod

---
+++ src/components/NewEntityPod.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Lixeira } from 'src/api/models/lixeira'
+import { createUseStyles } from 'react-jss'
+import SelectedEntityContext from 'src/components/contexts/SelectedEntityContext'
+
+const NewEntityPod : React.FC = () => {
+
+    const styles = useStyles()
+
+    function createNewLixeira(setSelected : (selected : Lixeira) => void) {
+        const lixeira : Lixeira = {
+            type : 'Feature',
+            geometry : {
+                type : 'Point',
+                coordinates : [null, null]
+            },
+            properties : {
+                location : null,
+                description : null,
+                capacity : null,
+                distanceBottom : null,
+                distanceCover : null
+            }
+        }
+        setSelected(lixeira)
+    }
+
+    return (
+        <SelectedEntityContext.Consumer>
+            {({selected, setSelected}) => (
+                <div className={styles.container} onClick={() => createNewLixeira(setSelected)}>
+                    Novo
+                </div>
+            )}
+        </SelectedEntityContext.Consumer>
+    )
+
+}
+
+export default NewEntityPod
+
+const useStyles = createUseStyles({
+    container : {
+		overflow : 'hidden',
+		display : 'flex',
+		borderRadius : '0.85em',
+		border : ['solid', 'lightgray', 1],
+		margin : ['1em', 0]
+	},
+})
