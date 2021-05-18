b1f7f08001db58d78a6883750bc478482d7a71a1
Angelo Netho <angelonetho@gmail.com>
2021-05-18 18:30:02 -0300

    Cria controller para a rota logCapacity

---
+++ src/api/controllers/logCapacity.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import Lixeira from '../models/lixeira';
+import LogCapacity from '../models/logCapacity';
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    try {
+        const log = await LogCapacity.create(req.body);
+        const lixeira = await Lixeira.findById(log.lixeira);
+        lixeira.properties.capacity = log.capacity;
+        await lixeira.save();
+        return res.send({ log });
+    } catch (err) {
+        return res.status(400).send({ error: 'Registration failed' });
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const query = await (LogCapacity.find({}))
+        return res.send(query);
+    }
+    catch (err) {
+        return res.status(500).send({ error: 'Query failed' });
+    }
+})
+
+export default app => app.use('/capacity', router);
