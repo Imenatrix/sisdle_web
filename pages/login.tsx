1cb745dadf52ea36b4eebac6a1925d46c5e36df3
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-06-28 16:31:07 -0300

    Adiciona tela de login

---
+++ pages/login.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import LoginCard from 'src/components/LoginCard'
+import { createUseStyles } from 'react-jss'
+import Map from 'src/components/Map'
+
+const Login : React.FC = () => {
+
+	const styles = useStyles()
+
+	return (
+		<div className={styles.container}>
+			<Map/>
+			<div className={styles.foreground}>
+				<LoginCard/>
+			</div>
+		</div>
+	)
+}
+
+export default Login
+
+const useStyles = createUseStyles({
+	'@global body' : {
+		margin : 0
+	},
+	'container' : {
+		'display' : 'flex',
+		'width' : '100vw',
+		'height' : '100vh'
+	},
+	foreground : {
+		display : 'flex',
+		position : 'absolute',
+		width : '100%',
+		height : '100%'
+	}
+})
