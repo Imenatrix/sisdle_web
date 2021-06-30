bc86a43a8c98e152017bc4643a634855cd5e9ddd
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-06-29 22:29:58 -0300

    Implementa Map com react-map-gl

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import { createUseStyles } from 'react-jss'
+import React, { useState } from 'react'
+import ReactMapGL from 'react-map-gl'
 
 const Map : React.FC = () => {
 
-	const styles = useStyles()
+	const [viewport, setViewport] = useState({
+		latitude: 37.7577,
+		longitude: -122.4376,
+		zoom: 8
+	})
 
 	return (
-		<div className={styles.container}>
-			{
-				// aqui vai o mapa (leaflet, Mapbox GL JS, etc)
-			}
-		</div>
+		<ReactMapGL
+			{...viewport}
+			width="100%"
+			height="100%"
+			mapStyle="mapbox://styles/mapbox/streets-v11"
+			onViewportChange={(viewport) => setViewport(viewport)}
+			mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}/>
 	)
 
 }
 
 export default Map
-
-const useStyles = createUseStyles({
-	container : {
-		backgroundColor : 'blue',
-		flex : 1
-	}
-})
