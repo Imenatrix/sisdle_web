f5b404ada12b95c7749040a8d6a0af4e2bacda9e
Angelo Netho <angelonetho@gmail.com>
2021-07-15 22:22:18 -0300

    Adapta para verificar o displayName sem modificar

--- src/api/controllers/adminController.ts
+++ src/api/controllers/adminController.ts
@@ -4,14 +4,14 @@ import Admin from '../models/admin';
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    const { name } = req.body;
+    const { displayName } = req.body;
 
     //Verifica se a req possui os dados necessários
-    if (!name) return res.status(400).send({ error: 'Dados Insuficientes' })
+    if (!displayName) return res.status(400).send({ error: 'Dados Insuficientes' })
 
     try {
         //Verifica se já existe um admin com o mesmo nome
-        if (await Admin.findOne({ name })) return res.send({ error: 'Nome ja registrado' })
+        if (await Admin.findOne({ idName: displayName.toLowerCase() })) return res.send({ error: 'Nome ja registrado' })
 
         const admin = await Admin.create(req.body);
         return res.send(admin);
