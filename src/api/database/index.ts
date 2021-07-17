82aa4ed2d85d2a690e946710a3249f4af0bdbd55
Angelo Netho <angelonetho@gmail.com>
2021-07-17 18:58:38 -0300

    Adiciona verificação de erros pós conexão inicial

--- src/api/database/index.ts
+++ src/api/database/index.ts
@@ -7,4 +7,9 @@ mongoose.connect('mongodb+srv://dbUser:' + process.env.MONGODB_PASSWORD + '@sisd
         useCreateIndex: true
     }).catch(error => console.log("\nFalha ao conectar-se com o banco de dados\n" + error + "\n"));
 
+//Verifica erros que acontecerem após a conexão incial
+mongoose.connection.on('error', error => {
+    console.log("\nAparentemente mongoose não está funcionando.\n" + error + "\n");
+});
+
 export default mongoose;
