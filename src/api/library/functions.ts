8e0d96f78b06b227afb4a896fc6376e3d188d8f3
Angelo Netho <angelonetho@gmail.com>
2021-07-17 19:55:37 -0300

    Adiciona função auxiliar para mensagens coloridas

--- src/api/library/functions.ts
+++ src/api/library/functions.ts
@@ -9,5 +9,15 @@ export function parseLixeiraToPlainObject(query) {
     return lixeiras
 }
 
+export function coloredMessage(msg, colorCode) {
+
+    /*  
+    Color Codes:
+    https://github.com/mochajs/mocha/blob/9e95d36e4b715380cef573014dec852bded3f8e1/lib/reporters/base.js#L48
+    */
+
+    return '\u001b[' + colorCode + 'm' + msg + '\u001b[0m'
+}
+
 
 
