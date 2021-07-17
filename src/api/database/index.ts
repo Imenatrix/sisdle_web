b7c178f39fc80547b87a48d9234da5c5b4e6ca24
Angelo Netho <angelonetho@gmail.com>
2021-07-17 19:09:37 -0300

    Adiciona cores em erros na log do console

--- src/api/database/index.ts
+++ src/api/database/index.ts
@@ -5,11 +5,12 @@ mongoose.connect('mongodb+srv://dbUser:' + process.env.MONGODB_PASSWORD + '@sisd
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
-    }).catch(error => console.log("\nFalha ao conectar-se com o banco de dados\n" + error + "\n"));
+    }).catch(error => console.log('\u001b[' + 31 + 'm' + "\nFalha ao conectar-se com o banco de dados\n" + error + "\n" + '\u001b[0m'));
 
 //Verifica erros que acontecerem após a conexão incial
 mongoose.connection.on('error', error => {
-    console.log("\nAparentemente mongoose não está funcionando.\n" + error + "\n");
+    var msg = "\nAparentemente mongoose não está funcionando.\n" + error + "\n"
+    console.log('\u001b[' + 31 + 'm' + msg + '\u001b[0m')
 });
 
 export default mongoose;
