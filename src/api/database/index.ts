efa40e41b5d63edada2eb6451bc0b8411f9ebdef
Angelo Netho <angelonetho@gmail.com>
2021-07-17 19:56:26 -0300

    Refatora o código para utilizar coloredMessage

--- src/api/database/index.ts
+++ src/api/database/index.ts
@@ -1,16 +1,16 @@
 import mongoose from 'mongoose';
+import { coloredMessage } from '../library/functions';
 
 mongoose.connect('mongodb+srv://dbUser:' + process.env.MONGODB_PASSWORD + '@sisdlecluster.e1e0p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
-    }).catch(error => console.log('\u001b[' + 31 + 'm' + "\nFalha ao conectar-se com o banco de dados\n" + error + "\n" + '\u001b[0m'));
+    }).catch(error => console.log(coloredMessage("\nFalha ao conectar-se com o banco de dados\n" + error + "\n", 31)));
 
 //Verifica erros que acontecerem após a conexão incial
 mongoose.connection.on('error', error => {
-    var msg = "\nAparentemente mongoose não está funcionando.\n" + error + "\n"
-    console.log('\u001b[' + 31 + 'm' + msg + '\u001b[0m')
+    console.log(coloredMessage("\nAparentemente mongoose não está funcionando.\n" + error + "\n", 31));
 });
 
 export default mongoose;
