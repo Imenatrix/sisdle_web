5799be1867314e147d8b57c26ecfc921718b7213
Angelo Netho <angelonetho@gmail.com>
2021-07-19 19:26:01 -0300

    Adiciona dicionario de cores

--- src/api/library/functions.ts
+++ src/api/library/functions.ts
@@ -13,7 +13,8 @@ export function coloredMessage(msg, colorCode) {
 
     /*  
     Color Codes:
-    https://github.com/mochajs/mocha/blob/9e95d36e4b715380cef573014dec852bded3f8e1/lib/reporters/base.js#L48
+    Green: 32
+    Red: 31
     */
 
     return '\u001b[' + colorCode + 'm' + msg + '\u001b[0m'
