a5985c2132fc9bd59be67d733e880bd93c2400c5
Angelo Netho <angelonetho@gmail.com>
2021-07-21 16:06:56 -0300

    Adiciona verificação diferenciada para o header

--- src/api/middlewares/auth.ts
+++ src/api/middlewares/auth.ts
@@ -3,10 +3,12 @@ import jwt = require('jsonwebtoken');
 const auth = (req, res, next) => {
     const bearerHeader = req.headers['authorization'];
 
-    if (!bearerHeader) return res.send({ error: 'Token ausente' });
+    if (!bearerHeader) return res.send({ error: 'Header ausente' });
 
     const bearer = bearerHeader.split(' ');
     const bearerToken = bearer[1];
+
+    if (!bearerToken) return res.send({ error: 'Token ausente' });
 
     try {
         res.locals.auth_data = jwt.verify(bearerToken, process.env.JWT_SECRETKEY);
