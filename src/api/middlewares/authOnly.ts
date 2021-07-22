90f8b858a203a719164fa2813f22ce7e2ead8ab9
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-07-22 17:29:23 -0300

    Remove logs de debug de authOnly

--- src/api/middlewares/authOnly.ts
+++ src/api/middlewares/authOnly.ts
@@ -2,8 +2,6 @@ import auth from './auth'
 
 function authOnly(urls : Array<string>) {
     return (req, res, next) => {
-        console.log(req.url)
-        console.log(urls)
         if (urls.includes(req.url)) {
             return auth(req, res, next)
         }
