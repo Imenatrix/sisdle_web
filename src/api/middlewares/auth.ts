73a6917b28dcb002cf1d33f0f0f94088b24b33b6
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-07-22 15:15:18 -0300

    Simplifica middleware auth usando express-jwt

--- src/api/middlewares/auth.ts
+++ src/api/middlewares/auth.ts
@@ -1,22 +1,11 @@
-import jwt = require('jsonwebtoken');
+import jwt from 'express-jwt'
 
 const auth = (req, res, next) => {
-    const bearerHeader = req.headers['authorization'];
-
-    if (!bearerHeader) return res.send({ error: 'Header ausente' });
-
-    const bearer = bearerHeader.split(' ');
-    const bearerToken = bearer[1];
-
-    if (!bearerToken) return res.send({ error: 'Token ausente' });
-
-    try {
-        res.locals.auth_data = jwt.verify(bearerToken, process.env.JWT_SECRETKEY);
-        return next();
-    } catch (err) {
-        return res.send({ error: 'Token inválido' })
-    }
-
+    return jwt({
+        secret : process.env.JWT_SECRETKEY,
+        getToken : req => req.cookies['auth_token'],
+        algorithms: ['HS256']
+    })(req, res, next)
 }
 
 export default auth;
