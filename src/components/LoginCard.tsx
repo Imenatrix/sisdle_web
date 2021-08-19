344f3f27c95a0812ff1afa75979923b29e606ca6
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-08-19 16:17:21 -0300

    Corrige fluxo de login e adiciona mensagem de erro

--- src/components/LoginCard.tsx
+++ src/components/LoginCard.tsx
@@ -1,18 +1,46 @@
 import React from 'react'
 import MainCard from './MainCard'
 import { createUseStyles } from 'react-jss'
+import { useState } from 'react'
 
 const LoginCard : React.FC = () => {
 
 	const styles = useStyles()
 
+	const [login, setLogin] = useState('')
+	const [password, setPassword] = useState('')
+	const [error, setError] = useState(false)
+
+	async function tryLogin() {
+		const res = await fetch('/user/login', {
+			method : 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body : JSON.stringify({
+				login : login,
+				password : password
+			})
+		})
+		if (res.redirected) {
+			window.location.href = res.url
+		}
+		else if(res.status >= 400) {
+			setError(true)
+		}
+	}
+
 	return (
 		<MainCard>
-			<form className={styles.content} action="/user/login" method="post">
-				<input className={styles.txt} type="text" name="login" placeholder="Login"/>
-				<input className={styles.txt} type="password" name="password" placeholder="Senha"/>
-				<button className={styles.btn} type="submit">Entrar</button>
-			</form>
+			<div className={styles.content}>
+				{error &&
+					<p className={styles.error}>Usuário ou senha incorretos.</p>
+				}
+				<input className={styles.txt} value={login} onChange={e => setLogin(e.target.value)} placeholder="Login"/>
+				<input className={styles.txt} value={password} onChange={e => setPassword(e.target.value)} placeholder="Senha"/>
+				<button className={styles.btn} onClick={tryLogin}>Entrar</button>
+			</div>
 		</MainCard>
 	)
 }
@@ -47,5 +75,10 @@ const useStyles = createUseStyles({
 		'&:focus' : {
 			outline : 'none'
 		}
+	},
+	error : {
+		color : '#f02849',
+		position : 'relative',
+		top : 0
 	}
 })
